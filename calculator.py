e9fc48588224f1302537670c73acb2b134e3804f
Khalid <khalid.csit1234@gmail.com>
2020-04-20 13:00:37 +0430

    2nd commited

--- calculator.py
+++ calculator.py
@@ -28,6 +28,8 @@ select = int(input("Select operations form 1, 2, 3, 4 :"))
   
 number_1 = int(input("Enter first number: ")) 
 number_2 = int(input("Enter second number: ")) 
+# i change here
+print('Hello this my msg');
   
 if select == 1: 
     print(number_1, "+", number_2, "=", 
