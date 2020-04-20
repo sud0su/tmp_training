0dfb5b8663f061f3123758e16c3fc6586fef0d50
Khalid <khalid.csit1234@gmail.com>
2020-04-20 14:53:30 +0430

    my test commit

--- calculator.py
+++ calculator.py
@@ -30,7 +30,7 @@ select = int(input(" ===========## Updated ##========== \n"
 number_1 = int(input("Enter first number: ")) 
 number_2 = int(input("Enter second number: ")) 
 # i change here
-print('this is edited by Finka');
+print('this is edited by Khalid Usman');
   
 # edited by dodi 
 
